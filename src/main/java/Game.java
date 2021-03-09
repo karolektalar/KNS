8eedf0279343e2e6ce1032069407e46551947882
karol <karol@puchaczy.waw.pl>
2021-03-09 11:26:31 +0100

    Działająca gra, z losową strategią

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,5 @@
-package main.java;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
 
 import java.util.Random;
 import java.util.Scanner;
@@ -18,7 +19,7 @@ public class Game {
             System.out.println("Wybierz miejsce w słowie w którym komputer ma postawić literę");
             String showPositions = "1";
             for (int i = 0; i < word.length(); i++) {
-                showPositions = showPositions + word.charAt(i) + (i+2);
+                showPositions = showPositions + word.charAt(i) + (i + 2);
             }
             System.out.println(showPositions);
             Scanner in = new Scanner(System.in);
@@ -39,12 +40,42 @@ public class Game {
             Random random = new Random();
             random.nextInt(alphabet.length);
             word = prefix + alphabet[random.nextInt(alphabet.length)] + suffix;
-            checkCorrectnessOfTheWord(word, alphabet);
+            if(checkWinner(word, alphabet)) {
+                System.out.println("Przegrałeś");
+                return;
+            }
+
+
         }
         System.out.println("Ostateczne słowo to: " + word);
+        System.out.println("Wygrałeś");
     }
 
-    private int checkCorrectnessOfTheWord(String word, char[] alphabet ) {
-        return 0;
+    private boolean checkWinner(String word, char[] alphabet) {
+        System.out.println("####################################################");
+        System.out.println("Sprawdzanie słowa: " + word);
+        //i mówi o początku słowa
+        for (int i = 0; i < word.length(); i++) {
+            //j mówi o długości słowa
+            for (int j = 1; j < word.length()/2 + 1; j++) {
+                if (j*2 + i <= word.length()) {
+                    Multiset<String> firstWord = HashMultiset.create();
+                    Multiset<String> secondWord = HashMultiset.create();
+                    for (int k = i; k < i + j; k++) {
+                        firstWord.add(String.valueOf(word.charAt(k)));
+                    }
+                    for (int k = i + j; k < i + 2*j; k++) {
+                        secondWord.add(String.valueOf(word.charAt(k)));
+                    }
+                    System.out.println(firstWord);
+                    System.out.println(secondWord);
+                    System.out.println("####################################################");
+                    if (firstWord.containsAll(secondWord)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
